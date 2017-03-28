37dc6f89ef60fb6ad4e8839ca483347c110dea20
CODEFYSOFTWARE\vladv <vlad.vasiliu@codefysoftware.com>
2017-03-28 16:03:52 +0300

    Add heat Map on locations

--- mapGoogle.js
+++ mapGoogle.js
@@ -2,25 +2,34 @@
 var map;
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 45.8207836, lng:24.8523799},
-        zoom: 7,
-        mapTypeId: 'terrain'
-    });
-    // var coords1 = 46.403586;
-    // var coords2 = 26.7307701;
-    // var latLng = new google.maps.LatLng(coords1,coords2);
-    // var marker = new google.maps.Marker({
-    //     position: latLng,
-    //     map: map
-    // 
-    statiiMeteo.forEach(function(statie){
-        var latLng = new google.maps.LatLng(statie.lat,statie.lon);
-        var marker = new google.maps.Marker({
-         position: latLng,
-         map: map
-    })
-})
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 45.8207836, lng:24.8523799},
+            zoom: 7,
+            mapTypeId: 'terrain'
+        });
+
+        var heatMapData =[];
+        statiiMeteo.forEach(function(statie,index){
+           var z = {
+               location: new google.maps.LatLng(statie.lat, statie.lon),
+               weight: 220
+           }
+           heatMapData.push(z);
+        })
+        console.log(heatMapData);
+        // statiiMeteo.forEach(function(statie){
+        //         var latLng = new google.maps.LatLng(statie.lat,statie.lon);
+        //         var marker = new google.maps.Marker({
+        //         position: latLng,
+        //         map: map
+        //     })
+        // })
+
+        var heatmap = new google.maps.visualization.HeatmapLayer({
+            data: heatMapData
+        });
+        
+        heatmap.setMap(map);
 }
 
 
