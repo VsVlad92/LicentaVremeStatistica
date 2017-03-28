d0a197754d4c5cec462a14ccd7d839e6bc1bfab4
CODEFYSOFTWARE\vladv <vlad.vasiliu@codefysoftware.com>
2017-03-28 16:21:19 +0300

    Am adaugat cercuri in statii , acestea vor avea clase diferite si marimi diferite in functie de statie meteo si temperatura

--- mapGoogle.js
+++ mapGoogle.js
@@ -11,8 +11,8 @@ function initMap() {
         var heatMapData =[];
         statiiMeteo.forEach(function(statie,index){
            var z = {
-               location: new google.maps.LatLng(statie.lat, statie.lon),
-               weight: 220
+               center: {lat: statie.lat, lng : statie.lon},
+               weight: 99999
            }
            heatMapData.push(z);
         })
@@ -25,11 +25,27 @@ function initMap() {
         //     })
         // })
 
-        var heatmap = new google.maps.visualization.HeatmapLayer({
-            data: heatMapData
-        });
+
+        // var heatmap = new google.maps.visualization.HeatmapLayer({
+        //     data: heatMapData
+        // });
         
-        heatmap.setMap(map);
+        //heatmap.setMap(map);
+        heatMapData.forEach(function(statie,index){
+          // Add the circle for this city to the map.
+          var cityCircle = new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: map,
+            center: statie.center,
+            radius: Math.sqrt(statie.weight) * 100
+          });
+         })
+
+
 }
 
 
