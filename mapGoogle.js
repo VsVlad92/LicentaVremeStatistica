4d0fd23a3e8a5b96ad80c2379379b8cd28e5b1a1
CODEFYSOFTWARE\vladv <vlad.vasiliu@codefysoftware.com>
2017-03-28 15:48:36 +0300

    Am adaugat pe harta toate statiile meteo

--- mapGoogle.js
+++ mapGoogle.js
@@ -1,7 +1,26 @@
+
 var map;
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: {lat: 45.8207836, lng:24.8523799},
+        zoom: 7,
+        mapTypeId: 'terrain'
     });
+    // var coords1 = 46.403586;
+    // var coords2 = 26.7307701;
+    // var latLng = new google.maps.LatLng(coords1,coords2);
+    // var marker = new google.maps.Marker({
+    //     position: latLng,
+    //     map: map
+    // 
+    statiiMeteo.forEach(function(statie){
+        var latLng = new google.maps.LatLng(statie.lat,statie.lon);
+        var marker = new google.maps.Marker({
+         position: latLng,
+         map: map
+    })
+})
 }
+
+
---
+++ statiimeteo.js
@@ -0,0 +1,141 @@
+// 15015 - Ocna Sugatag - 47.7839696  23.942374
+// 15020-Botosani - 47.7391831 26.6707378
+// 15090-Iasi - 47.1572832 27.5979553
+// 15108-Ceahlau Toaca - 46.9776171 25.9497299
+// 15120-Cluj-Napoca - 46.7697272 23.6086146
+// 15150-Bacau - 46.5578835 26.9136647
+// 15170-Miercurea Ciuc - 46.3564998 25.7795527
+// 15200-Arad - 46.1753146 21.3075592
+// 15230-Deva - 45.8652473 22.9248964
+// 15260-Sibiu - 45.7801025 24.1212973
+// 15280-Varfu Omu - 45.44562 25.4574466
+// 15292-Caransebes - 45.4095726  22.2136228
+// 15310-Galati - 45.4334964 28.0207153
+// 15335-Tulcea - 45.1793693 28.7813096
+// 15346-Ramnicu Valcea - 45.0998868 24.3538567
+// 15350-Buzau - 45.147066 26.8179698
+// 15360-Sulina - 45.1536732 29.6512413
+// 15410-Drobeta Turnu Severin - 44.6330721 22.6578426
+// 15420-Bucuresti-Baneasa - 44.433882 26.0853663
+// 15450-Craiova - 44.323525 23.7846919
+// 15460-Calarasi - 44.2015769 27.3175526
+// 15470-Rosiorii de Vede - 44.1039154 24.9919223
+// 15480-Constanta - 44.1765783 28.6083948
+
+var statiiMeteo = [];
+statiiMeteo[15015]={
+    name: "Ocna Sugatag",
+    lat: 47.7839696,
+    lon: 23.942374
+}
+statiiMeteo[15020]={
+    name: "Botosani",
+    lat: 47.7391831,
+    lon: 26.6707378
+}
+statiiMeteo[15090]={
+    name: "Iasi",
+    lat: 47.1572832,
+    lon: 27.5979553
+}
+statiiMeteo[15108]={
+    name: "Ceahlau Toaca",
+    lat: 46.9776171,
+    lon: 25.9497299
+}
+statiiMeteo[15120]={
+    name: "Cluj-Napoca",
+    lat: 46.7697272,
+    lon: 23.6086146
+}
+statiiMeteo[15150]={
+    name: "Bacau",
+    lat: 46.5578835,
+    lon: 26.9136647
+}
+statiiMeteo[15170]={
+    name: "Miercurea Ciuc",
+    lat: 46.3564998,
+    lon: 25.7795527
+}
+statiiMeteo[15200]={
+    name: "Arad",
+    lat: 46.1753146,
+    lon: 21.3075592
+}
+statiiMeteo[15230]={
+    name: "Deva",
+    lat:  45.8652473,
+    lon: 22.9248964
+}
+statiiMeteo[15260]={
+    name: "Sibiu",
+    lat: 45.7801025,
+    lon: 24.1212973
+}
+statiiMeteo[15280]={
+    name: "Varfu Omu",
+    lat: 45.44562,
+    lon: 25.4574466
+}
+statiiMeteo[15292]={
+    name: "Caransebes",
+    lat: 45.4095726,
+    lon: 22.2136228
+}
+statiiMeteo[15310]={
+    name: "Galati",
+    lat: 45.4334964,
+    lon: 28.0207153
+}
+statiiMeteo[15335]={
+    name: "Tulcea",
+    lat: 45.1793693,
+    lon: 28.7813096
+}
+statiiMeteo[15346]={
+    name: "Ramnicu Valcea",
+    lat: 45.0998868,
+    lon: 24.3538567
+}
+statiiMeteo[15350]={
+    name: "Buzau",
+    lat: 45.147066,
+    lon: 26.8179698
+}
+statiiMeteo[15360]={
+    name: "Sulina",
+    lat: 45.1536732,
+    lon: 29.6512413
+}
+statiiMeteo[15410]={
+    name: "Drobeta Turnu Severin",
+    lat: 44.6330721,
+    lon: 22.6578426
+}
+statiiMeteo[15420]={
+    name: "Bucuresti-Baneasa",
+    lat: 44.433882,
+    lon: 26.0853663
+}
+statiiMeteo[15450]={
+    name: "Craiova",
+    lat: 44.323525,
+    lon: 23.7846919
+}
+statiiMeteo[15460]={
+    name: "Calarasi",
+    lat: 44.2015769,
+    lon: 27.3175526
+}
+statiiMeteo[15470]={
+    name: "Rosiorii de Vede",
+    lat: 44.1039154,
+    lon: 24.9919223
+}
+statiiMeteo[15480]={
+    name: "Constanta",
+    lat: 44.1765783,
+    lon: 28.6083948
+}
+
